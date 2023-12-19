98cfe416aa9f0af018adde93b7703b273bf380f9
AryanVarma2021 <varmaaryan11@gmail.com>
2023-12-19 22:12:48 +0530

    Setup of cloudinary and multer environment

---
+++ src/utils/cloudnary.js
@@ -0,0 +1,30 @@
+import {v2 as cloudinary} from "cloudinary"
+import fs from "fs"
+import { env } from "process"
+
+
+cloudinary.config({
+    cloud_name : process.env.CLOUDNAME ,
+    api_key : process.env.CLOUDNARY_API_KEY,
+    api_secret : process.env.CLOUDNARY_API_SECRET_KEY
+});
+
+
+
+const uploadonCloudinary = async (localfilepath) =>{
+    try {
+        if(!localfilepath) return "Upload file";
+
+        const response = await cloudinary.uploader.upload(localfilepath, {resource_type:"auto"})
+
+        console.log("File uploaded successfully", response.url);
+        return response;
+
+        
+    } catch (error) {
+        fs.unlinkSync(localfilepath)
+        console.log("ERROR : ", error );
+        
+    }
+
+}
