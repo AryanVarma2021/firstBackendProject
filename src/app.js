3417719d954d2bbc570859edd9a2b64a7234caaa
AryanVarma2021 <varmaaryan11@gmail.com>
2023-12-20 17:23:13 +0530

    makes user routes setup

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 
-export const app = express();
+ const app = express();
 
 //Middlewares
 app.use(
@@ -13,9 +13,22 @@ app.use(
 );
 
 app.use(express.json({ limit: "16kb" }));
-
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
-
 app.use(express.static("public"));
-
 app.use(cookieParser());
+
+
+//routes import
+import userRouter from "./routes/user.routes.js"
+
+app.use("/api/v1/users", userRouter)
+
+
+export default app;
+
+
+
+
+
+
+
